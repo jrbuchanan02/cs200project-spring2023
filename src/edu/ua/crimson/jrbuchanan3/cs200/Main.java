1ce49d1f074e8056ff23f71490ccd6048690debe
Joshua Buchanan <62662095+jrbuchanan02@users.noreply.github.com>
2023-01-25 14:31:12 -0600

    Sort of an initial commit.

---
+++ src/edu/ua/crimson/jrbuchanan3/cs200/Main.java
@@ -0,0 +1,16 @@
+package edu.ua.crimson.jrbuchanan3.cs200;
+/**
+ * This file contains the entry point for the project. Even though nothing will be here
+ * for a while, I still made the file because it will exist because our program is a 
+ * Java program.
+ * @author Joshua Buchanan
+ * @since 1/25/2023
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
